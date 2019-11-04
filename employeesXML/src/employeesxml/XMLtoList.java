a9fd3979301e82bbafeba9f51c7e336830c820aa
Matteo-x220 <matteosasoris@gmail.com>
2019-11-04 09:23:03 +0100

    EmployeesXML

---
+++ employeesXML/src/employeesxml/XMLtoList.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package employeesxml;
+
+import java.io.File;
+import java.io.IOException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+/**
+ *
+ * @author matteo
+ */
+public class XMLtoList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static NodeList getTree(String filename) throws ParserConfigurationException, SAXException, IOException {
+        // TODO code application logic here
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(new File(filename));
+        document.getDocumentElement().normalize();
+        Element root = document.getDocumentElement();
+        //System.out.println(root.getNodeName());
+        NodeList nList = document.getElementsByTagName("employee");
+        return nList;
+    }
+
+    public String toString(NodeList nList) {
+        String out = "";
+        for (int temp = 0; temp < nList.getLength(); temp++) {
+            Node node = nList.item(temp);
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                Element eElement = (Element) node;
+                out += ("ID impiegato: " + eElement.getAttribute("id"))
+                        + ("Nome: " + eElement.getElementsByTagName("firstName").item(0).getTextContent()) + "\n"
+                        + ("Cognome: " + eElement.getElementsByTagName("lastName").item(0).getTextContent()) + "\n"
+                        + ("Luogo: " + eElement.getElementsByTagName("location").item(0).getTextContent()) + "\n";
+                /*System.out.println("ID impiegato: " + eElement.getAttribute("id"));
+                System.out.println("Nome: " + eElement.getElementsByTagName("firstName").item(0).getTextContent());
+                System.out.println("Cognome: " + eElement.getElementsByTagName("lastName").item(0).getTextContent());
+                System.out.println("Luogo: " + eElement.getElementsByTagName("location").item(0).getTextContent());*/
+            }
+        }
+        return out;
+    }
+}
