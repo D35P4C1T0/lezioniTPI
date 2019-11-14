f374b0b6bae0609794d782ce58778ac143b43933
Matteo-x220 <matteosasoris@gmail.com>
2019-11-14 20:51:25 +0100

    Corretto parsing degli URL delle immagini di unsplash e inserimento nella pagina web

--- unsplashJSONJS/app.js
+++ unsplashJSONJS/app.js
@@ -1,18 +1,19 @@
-const urls = document.getElementById('urls')
-const loadingButton = document.getElementById('loadingButton')
-
-
+const urlList = document.getElementById("urlList")
+const loadingButton = document.getElementById("loadingButton")
 
 const accessKey =
-    'd069cc2dd9e584b20a67073c45d5d1a037cddac68c067e9ff32442eabc862df4'
+  "d069cc2dd9e584b20a67073c45d5d1a037cddac68c067e9ff32442eabc862df4"
 
 const loadData = () => {
-  fetch('https://api.unsplash.com/photos/?client_id=' + accessKey)
-      .then(response => response.json())
-      .then(body => {
-        body.forEach(element => {const li = document.createElement('li')
-    li.innerHTML = '<img> src="'} + );
+  fetch("https://api.unsplash.com/photos/?client_id=" + accessKey)
+    .then(response => response.json())
+    .then(body => {
+      body.forEach(element => {
+        const li = document.createElement("li")
+        li.innerHTML = '<img src="' + element.links.download + '">'
+        urlList.appendChild(li)
       })
-};
+    })
+}
 
 loadingButton.onclick = loadData
