938e9ecbe09d020988da993455663217f2d56a22
Matteo-x220 <matteosasoris@gmail.com>
2019-11-14 22:32:36 +0100

    further styling

--- unsplashJSONJS/app.js
+++ unsplashJSONJS/app.js
@@ -1,19 +1,24 @@
-const urlList = document.getElementById("urlList")
-const loadingButton = document.getElementById("loadingButton")
+const urlList = document.getElementById('urlList')
+const loadingButton = document.getElementById('loadingButton')
+const searchBar = document.getElementById('searchBar')
+let searchTerm = searchBar.value
 
-const accessKey =
-  "d069cc2dd9e584b20a67073c45d5d1a037cddac68c067e9ff32442eabc862df4"
+const client_id =
+    'd069cc2dd9e584b20a67073c45d5d1a037cddac68c067e9ff32442eabc862df4'
 
 const loadData = () => {
-  fetch("https://api.unsplash.com/photos/?client_id=" + accessKey)
+  urlList.innerHTML = ''
+  searchTerm = document.getElementById('searchBar').value
+  fetch('https://api.unsplash.com/search/photos?client_id=' + client_id + '&query=' + searchTerm)
     .then(response => response.json())
-    .then(body => {
-      body.forEach(element => {
-        const li = document.createElement("li")
-        li.innerHTML = '<img src="' + element.links.download + '">'
+    .then(data => {
+      data.results.forEach(element => {
+        const li = document.createElement('li')
+  li.innerHTML = '<img src="' + element.urls.regular + '">'
         urlList.appendChild(li)
       })
     })
-}
+};
 
 loadingButton.onclick = loadData
+searchBar.onchange = loadData
