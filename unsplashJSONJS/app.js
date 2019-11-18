98baacb37ae8a7f27d1093ed699b3a550803995c
Matteo-x220 <matteosasoris@gmail.com>
2019-11-18 09:45:57 +0100

    Async fetch

--- unsplashJSONJS/app.js
+++ unsplashJSONJS/app.js
@@ -1,30 +1,29 @@
-const urlList = document.getElementById("urlList")
-const loadingButton = document.getElementById("loadingButton")
-const searchBar = document.getElementById("searchBar")
+const urlList = document.getElementById('urlList')
+const loadingButton = document.getElementById('loadingButton')
+const searchBar = document.getElementById('searchBar')
 
 const client_id =
-  "d069cc2dd9e584b20a67073c45d5d1a037cddac68c067e9ff32442eabc862df4"
+    'd069cc2dd9e584b20a67073c45d5d1a037cddac68c067e9ff32442eabc862df4'
 
-const loadData = () => {
-  urlList.innerHTML = ""
+const loadData =
+    async () => {
+  urlList.innerHTML = ''
   let searchTerm = searchBar.value
-  fetch(
-    "https://api.unsplash.com/search/photos?client_id=" +
-      client_id +
-      "&query=" +
-      searchTerm
-  )
-    .then(response => response.json())
-    .then(data => {
+  let data =
+      await fetch(
+          'https://api.unsplash.com/search/photos?client_id=' + client_id +
+          '&query=' + searchTerm)
+          .then(response => response.json());
+
       data.results.forEach(element => {
         console.log(element)
-        const li = document.createElement("li")
-        li.innerHTML = '<img src="' + element.urls.regular + '">'
+      const li = document.createElement('li')
+      li.innerHTML = '<img src="' + element.urls.regular + '">'
         urlList.appendChild(li)
-      })
-    })
+      });
 }
 
-loadingButton.onclick = loadData
+                loadingButton.onclick = loadData
 searchBar.onchange = loadData
-//searchBar.oninput = loadData //ocio che esaurisci le richieste giornaliere
+    // searchBar.oninput = loadData //ocio che esaurisci le richieste
+    // giornaliere
